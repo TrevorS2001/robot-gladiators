c26b0b950cf4568be2728d0ea423652109ba1279
Trevor Snell <trevorsnell@optonline.net>
2022-03-15 12:32:12 -0400

    "added each combatant"

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,13 +3,17 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function(){
+for (var i =0; i<enemyNames.length; i++){
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] +" is at " +i + " index");
+}
+
+var fight = function(enemyName){
     window.alert("Welcome to Robot Gladiators!");
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose");
@@ -60,4 +64,6 @@ else{
 }
 }
 
-fight();
+for (var i=0 ; i < enemyNames.length; i++){
+  fight(enemyNames[i]);
+}
